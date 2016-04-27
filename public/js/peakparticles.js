ccd43a0436f6e05c3ed5420472b6590a5984b158
Chris <cpachomski@gmail.com>
2016-04-27 18:03:11 -0400

    remove styles and use pasted code

--- public/js/peakparticles.js
+++ public/js/peakparticles.js
@@ -1,79 +1,239 @@
-console.log('running');
 
-var stats,
-	camera,
-	controls,
-	scene,
-	renderer,
-	objects = [],
-	plane,
-	world;
+			var container, stats;
+			var camera, controls, scene, renderer;
+			var objects = [], plane;
 
+			var raycaster = new THREE.Raycaster();
+			var mouse = new THREE.Vector2(),
+			offset = new THREE.Vector3(),
+			INTERSECTED, SELECTED;
 
-function createScene() {
-	//creating scene
-	world = document.getElementById('world')
-	scene = new THREE.Scene();
+			init();
+			animate();
 
+			function init() {
 
-	//add lighting
-	scene.add( new THREE.AmbientLight( 0x030303 ) );
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
 
-	var light = new THREE.SpotLight( 0xffffff, 1.5 );
-	light.position.set( 0, 400, 2000 );
-	light.castShadow = true;
+				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
+				camera.position.z = 1000;
 
-	light.shadow = new THREE.LightShadow ( new THREE.PerspectiveCamera ( 50, 1, 200, 10000 ) );
-	light.shadow.bias = -0.00022;
-	light.shadow.mapSize.height = 2048;
-	light.shadow.mapSize.width = 2048;
+				controls = new THREE.TrackballControls( camera );
+				controls.rotateSpeed = 1.0;
+				controls.zoomSpeed = 1.2;
+				controls.panSpeed = 0.8;
+				controls.noZoom = false;
+				controls.noPan = false;
+				controls.staticMoving = true;
+				controls.dynamicDampingFactor = 0.3;
 
-	scene.add(light);
+				scene = new THREE.Scene();
 
-	
+				scene.add( new THREE.AmbientLight( 0x505050 ) );
 
-	for (var i = 0; i < 200; i++) {
+				var light = new THREE.SpotLight( 0xffffff, 1.5 );
+				light.position.set( 0, 500, 2000 );
+				light.castShadow = true;
 
-		var sphereRadius = Math.random() * 10
-		var geometry = new THREE.SphereBufferGeometry( sphereRadius, 32, 32 );
-		var mat = new THREE.MeshLambertMaterial ( { color: 0x333333 } );
-		var sphere = new THREE.Mesh( geometry, mat );
+				light.shadow = new THREE.LightShadow( new THREE.PerspectiveCamera( 50, 1, 200, 10000 ) );
+				light.shadow.bias = - 0.00022;
 
-		sphere.position.x = Math.random() * 1000 - 500;
-		sphere.position.y = Math.random() * 600 - 300;
-		sphere.position.z = Math.random() * 800 - 400;
+				light.shadow.mapSize.width = 2048;
+				light.shadow.mapSize.height = 2048;
 
-		sphere.castShadow = true;
-		sphere.receiveShadow = true;
+				scene.add( light );
 
-		scene.add(sphere);
-		objects.push(sphere);
-	}
+				var geometry = new THREE.BoxGeometry( 40, 40, 40 );
 
-	plane = new THREE.Mesh(
-		new THREE.PlaneBufferGeometry( 2000, 2000, 8, 8),
-		new THREE.MeshBasicMaterial( { visible: false } )
-	);
+				for ( var i = 0; i < 200; i ++ ) {
 
-	scene.add(plane)
+					var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
 
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setClearColor(0xf0f0f0);
-	renderer.setPixelRatio( window.devicePixelRatio);
-	renderer.setSize( window.innerWidth, window.innerHeight);
-	renderer.sortObjects = false;
+					object.position.x = Math.random() * 1000 - 500;
+					object.position.y = Math.random() * 600 - 300;
+					object.position.z = Math.random() * 800 - 400;
 
-	renderer.shadowMap.enabled = true;
-	renderer.shadowMap.type = THREE.PCFShadowMap;
+					object.rotation.x = Math.random() * 2 * Math.PI;
+					object.rotation.y = Math.random() * 2 * Math.PI;
+					object.rotation.z = Math.random() * 2 * Math.PI;
 
-	world.appendChild( renderer.domElement );
+					object.scale.x = Math.random() * 2 + 1;
+					object.scale.y = Math.random() * 2 + 1;
+					object.scale.z = Math.random() * 2 + 1;
 
-	renderer.domElement.addEventListener( 'mousemove', handleMouseMove, false);
-	renderer.domElement.addEventListener( 'mouseDown', handleMouseDown, false);
-	renderer.domeElement.addEventListener( 'mouseup', handleMouseUp, false);
-}
+					object.castShadow = true;
+					object.receiveShadow = true;
 
+					scene.add( object );
 
+					objects.push( object );
 
-createScene();
+				}
 
+				plane = new THREE.Mesh(
+					new THREE.PlaneBufferGeometry( 2000, 2000, 8, 8 ),
+					new THREE.MeshBasicMaterial( { visible: false } )
+				);
+				scene.add( plane );
+
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setClearColor( 0xf0f0f0 );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.sortObjects = false;
+
+				renderer.shadowMap.enabled = true;
+				renderer.shadowMap.type = THREE.PCFShadowMap;
+
+				container.appendChild( renderer.domElement );
+
+				var info = document.createElement( 'div' );
+				info.style.position = 'absolute';
+				info.style.top = '10px';
+				info.style.width = '100%';
+				info.style.textAlign = 'center';
+				info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> webgl - draggable cubes';
+				container.appendChild( info );
+
+				stats = new Stats();
+				container.appendChild( stats.dom );
+
+				renderer.domElement.addEventListener( 'mousemove', onDocumentMouseMove, false );
+				renderer.domElement.addEventListener( 'mousedown', onDocumentMouseDown, false );
+				renderer.domElement.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+				//
+
+				window.addEventListener( 'resize', onWindowResize, false );
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			function onDocumentMouseMove( event ) {
+
+				event.preventDefault();
+
+				mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+				mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+				//
+
+				raycaster.setFromCamera( mouse, camera );
+
+				if ( SELECTED ) {
+
+					var intersects = raycaster.intersectObject( plane );
+
+					if ( intersects.length > 0 ) {
+
+						SELECTED.position.copy( intersects[ 0 ].point.sub( offset ) );
+
+					}
+
+					return;
+
+				}
+
+				var intersects = raycaster.intersectObjects( objects );
+
+				if ( intersects.length > 0 ) {
+
+					if ( INTERSECTED != intersects[ 0 ].object ) {
+
+						if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+
+						INTERSECTED = intersects[ 0 ].object;
+						INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+
+						plane.position.copy( INTERSECTED.position );
+						plane.lookAt( camera.position );
+
+					}
+
+					container.style.cursor = 'pointer';
+
+				} else {
+
+					if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+
+					INTERSECTED = null;
+
+					container.style.cursor = 'auto';
+
+				}
+
+			}
+
+			function onDocumentMouseDown( event ) {
+
+				event.preventDefault();
+
+				raycaster.setFromCamera( mouse, camera );
+
+				var intersects = raycaster.intersectObjects( objects );
+
+				if ( intersects.length > 0 ) {
+
+					controls.enabled = false;
+
+					SELECTED = intersects[ 0 ].object;
+
+					var intersects = raycaster.intersectObject( plane );
+
+					if ( intersects.length > 0 ) {
+
+						offset.copy( intersects[ 0 ].point ).sub( plane.position );
+
+					}
+
+					container.style.cursor = 'move';
+
+				}
+
+			}
+
+			function onDocumentMouseUp( event ) {
+
+				event.preventDefault();
+
+				controls.enabled = true;
+
+				if ( INTERSECTED ) {
+
+					plane.position.copy( INTERSECTED.position );
+
+					SELECTED = null;
+
+				}
+
+				container.style.cursor = 'auto';
+
+			}
+
+			//
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				render();
+				stats.update();
+
+			}
+
+			function render() {
+
+				controls.update();
+
+				renderer.render( scene, camera );
+
+			}
